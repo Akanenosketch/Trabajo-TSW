67712e3eb46a6a0fa870a56c613f71d9a505db54
DanielFabianRL <128245673+DanielFabianRL@users.noreply.github.com>
2025-11-13 15:30:46 +0100

    messages_en
    Cambios menores a los html, internacionalizacion acabada para lo actual

---
+++ EstructuraFinal/Codigo Fuente/view/messages/messages_en.php
@@ -0,0 +1,78 @@
+<?php
+//file: /view/messages/messages_en.php
+
+$i18n_messages = 
+array(
+	//login.php
+	"Pagina de Bienvenida" => "Welcome Page",
+	"Bienvenido" => "Welcome",
+	"Una app de administracion al estilo Kanban" => "A Kanban style manager app",
+	"Informacion:" => "Information:",
+	"Proyecto Trabajo-TSW — Interfaz de Ejemplo." => "TSW-Work Project — Example Interface.",
+	"Contacto: equipo@example.com · Version 1.0" => "Contact: equipo@example.com · Version 1.0",
+	
+
+
+	//login.php & login_form
+	"Iniciar sesion" => "Login",
+	
+	//login.php & register_form
+	"Registrarse" => "Register",
+	
+	//login_form & register_form
+	"Nombre de Usuario" => "Username",
+	"Contraseña" => "Password",
+
+	//login_form & register_form & project_form
+	"Cancelar" => "Cancel",
+	
+	// login_form
+	"Entrar" => "Enter",
+	
+	//register_form
+	"Correo" => "Email",
+	"Crear cuenta" => "Create Account",
+
+	//dashboard.php
+	"Dashboard" => "Dashboard",
+	"Usuario" => "User",
+	"Lista de Proyectos" => "Project List",
+	"Cerrar sesion" => "Logout",
+	"Añadir Proyecto" => "Add Project",
+	"Nombre del Proyecto" => "Project Name",
+	"Tareas ToDo" => "ToDo Tasks",
+	"Tareas Working" => "Working Tasks",
+	"Tareas Done" => "Done Tasks",
+	
+	//project_form
+	"Nuevo Proyecto" => "New Project",
+	"Crear proyecto" => "Create Project",
+	
+	//project.php project_form
+	"Participantes" => "Participants",
+	
+	//project.php
+	"Proyecto" => "Project",
+	"Tareas Totales" => "Total Tasks",
+	"Completado" => "Completed",
+	"Volver" => "Back",
+	"ToDo" => "ToDo",
+	"Working" => "Working",
+	"Done" => "Done",
+	"+ Nueva Tarea" => "+ New Task",
+	"Editar Proyecto" => "Edit Project",
+	"Eliminar Proyecto" => "Delete Project",
+	"Añadir Tarea" => "Add Task",
+	"Nombre" => "Name",
+	"Descripcion" => "Description",
+	"Estado" => "Status",
+	"Asignar a" => "Assign to",
+	"Guardar" => "Save",
+	"Administrar Usuarios" => "Handle Users",
+	"Usuarios Existentes" => "Existing Users",
+	"Añadir Nuevo Usuario" => "Add New User",
+	"Guardar Cambios" => "Save changes",
+	"¿Estás seguro de que deseas eliminar este proyecto? Esta acción no se puede deshacer y se perderán todas las tareas asociadas." 
+	=> "Are you sure you wish to delete this project? This action cannot be undone and all associated tasks will be lost."
+)
+?>
