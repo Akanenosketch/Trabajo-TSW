a6e1dd6731fffeb2387e4e2ea34a455b7bf87781
Akanenosketch <90091466+Akanenosketch@users.noreply.github.com>
2025-11-24 17:31:50 +0100

    routes not routing

--- EstructuraFinal/Codigo Fuente/view/projects/index.php
+++ EstructuraFinal/Codigo Fuente/view/projects/index.php
@@ -1,6 +1,6 @@
 <?php
 //file: view/projects/index.php
-require_once(__DIR__ . "/../../core/ViewManager.php");
+require_once(__DIR__."/../../core/ViewManager.php");
 $view = ViewManager::getInstance();
 
 $projects = $view->getVariable("projects");
@@ -14,7 +14,7 @@ $currentuserMail = $view->getVariable("currentusermail");
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <link rel="stylesheet" type="text/css" href="../style/project_index.css">
+    <link rel="stylesheet" type="text/css" href=./view/css/projects/index.css>
     <title><?= i18n("Dashboard") ?></title>
 </head>
 
