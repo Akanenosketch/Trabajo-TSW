3942c857aaaf614c276ff6ddc9ee777421c50938
DanielFabianRL <128245673+DanielFabianRL@users.noreply.github.com>
2025-11-30 13:48:30 +0100

    Update TasksController.php

--- EstructuraFinal/Codigo Fuente/controller/TasksController.php
+++ EstructuraFinal/Codigo Fuente/controller/TasksController.php
@@ -147,7 +147,7 @@ class TaskController extends BaseController
 	 */
 	public function add()
 	{
-		if ($this->currentUser != null) {
+		if ($this->currentUser == null) {
 			throw new Exception("Not in session. Adding tasks requires login");
 		}
 		if (!isset($_REQUEST["id"])) {
