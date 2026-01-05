14980fa6b5f6793b4b6c63459bc7dc8ab66e7cca
DanielFabianRL <dfabianrl2114@gmail.com>
2026-01-05 17:39:07 +0100

    Create ProjectsModel.js

---
+++ Codigo/frontend/js/model/ProjectsModel.js
@@ -0,0 +1,29 @@
+class ProjectsModel extends Fronty.Model {
+
+  constructor() {
+    super('ProjectsModel'); //call super le pone nombre al Model 
+
+    // model attributes
+    this.projects = [];
+  }
+
+  /**
+   * Asigna el projecto a ver/editar/añadir
+   * @param {*} project El ProjectModel sobre el que se opera
+   */
+  setSelectedProject(project) {
+    this.set((self) => {
+      self.setSelectedProject = project;
+    });
+  }
+
+  /**
+   * Asigna la lista de projectos a mostrar
+   * @param projects Una lista de ProjectModels 
+   */
+  setProjects(projects) {
+    this.set((self) => {
+      self.projects = projects;
+    });
+  }
+}
