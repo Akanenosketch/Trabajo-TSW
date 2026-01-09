1ca83830384317aa5294b65c883314e2e899da5e
DanielFabianRL <dfabianrl2114@gmail.com>
2026-01-09 16:36:09 +0100

    Update CategoryService.js

--- Codigo/frontend/js/service/CategoryService.js
+++ Codigo/frontend/js/service/CategoryService.js
@@ -4,10 +4,57 @@ class CategoryService {
         this.baseUrl = AppConfig.backendServer + 'categories/'
     }
 
-    //TODO AMPLIAR
+    /**
+     * Creates the category given
+     * @param category the category to create 
+     * @returns 
+     */
+    createCategory(category) {
+        return $.ajax({
+            url: this.baseUrl,
+            method: 'POST',
+            data: JSON.stringify(category),
+            contentType: 'application/json'
+        });
+    }
 
     /**
-    * Lists the emails of all existing users. 
+     * Gets the category with the given name
+     * @param catname the name of category to retrieve 
+     * @returns 
+     */
+    getCategory(catname) {
+        return $.get(this.baseUrl + catname);
+    }
+
+     /**
+     * Updates the category with given name
+     * @param catName the name of the category 
+     * @param category the category to update
+     */
+    updateCategory(catName,category){
+        return $.ajax({
+            url: this.baseUrl + catName,
+            method: 'PUT',
+            data: JSON.stringify(category),
+            contentType: 'application/json'
+        });
+    }
+
+    /**
+     * Deletes the category with given name
+     * @param catName the name of the category to delete 
+     * @returns 
+     */
+    deleteCategory(catName) {
+        return $.ajax({
+            url: this.baseUrl + catName,
+            method: 'DELETE'
+        });
+    }
+
+    /**
+    * Lists the category names of all existing cats. 
     */
     listAllCats(){
         return $.get(this.baseUrl);
