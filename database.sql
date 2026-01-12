6487e4c580ddc2405aca3a4159b1d16868104f2a
Meteoro214 <128245673+Meteoro214@users.noreply.github.com>
2026-01-12 21:53:59 +0100

    Tomorrow Comes

--- database.sql
+++ database.sql
@@ -87,12 +87,18 @@ CREATE TABLE users_on_tasks (
 -- Introducir datos de prueba en la base de datos
 INSERT INTO users (user_mail,username, passwd) VALUES ('tsw@uvigo.es', 'tswuser','tswpass');
 INSERT INTO users (user_mail,username, passwd) VALUES ('tsw2@uvigo.es', 'tswuser2','tswpass2');
+INSERT INTO users (user_mail,username, passwd) VALUES ('expedition33@expedition.fr', 'expedition33','Expedition33pass');
+
 
 INSERT INTO projects (project_name) VALUES ('TSWTestproject');
 INSERT INTO projects (project_name) VALUES ('TSWTestproject2');
+INSERT INTO projects (project_name) VALUES ('Expedition33');
+
 INSERT INTO users_on_projects (user_mail,project_id) VALUES ('tsw@uvigo.es',1);
 INSERT INTO users_on_projects (user_mail,project_id) VALUES ('tsw@uvigo.es',2);
 INSERT INTO users_on_projects (user_mail,project_id) VALUES ('tsw2@uvigo.es',1);
+INSERT INTO users_on_projects (user_mail,project_id) VALUES ('tsw2@uvigo.es',1);
+INSERT INTO users_on_projects (user_mail,project_id) VALUES ('expedition33@expedition.fr',3);
 
 INSERT INTO tasks (task_name, project_id, task_status, task_desc, task_priority, begin_date, end_date) 
 VALUES ('TSWTestToDO', 1, "ToDo", "desc", "High", '2024-01-01', '2024-01-10');
@@ -121,6 +127,10 @@ VALUES ('TSWTestWork', 2, "Working", "desc5", "High", '2024-02-05', '2024-02-15'
 INSERT INTO tasks (task_name, project_id, task_status, task_desc, task_priority, begin_date, end_date) 
 VALUES ('TSWTestDone', 2, "Done", "desc6", "Low", '2024-02-10', '2024-02-20');
 
+INSERT INTO tasks (task_name, project_id, task_status, task_desc, task_priority, begin_date, end_date) 
+VALUES ('Expedition 33', 3, "Done", "Expedition 33", "High", '2025-04-26', '2025-12-31');
+
+
 INSERT INTO users_on_tasks (user_mail,project_id,task_id) VALUES ('tsw@uvigo.es',1,1);
 INSERT INTO users_on_tasks (user_mail,project_id,task_id) VALUES ('tsw@uvigo.es',1,2);
 INSERT INTO users_on_tasks (user_mail,project_id,task_id) VALUES ('tsw@uvigo.es',1,3);
@@ -131,6 +141,7 @@ INSERT INTO users_on_tasks (user_mail,project_id,task_id) VALUES ('tsw@uvigo.es'
 INSERT INTO users_on_tasks (user_mail,project_id,task_id) VALUES ('tsw@uvigo.es',2,7);
 INSERT INTO users_on_tasks (user_mail,project_id,task_id) VALUES ('tsw@uvigo.es',2,8);
 INSERT INTO users_on_tasks (user_mail,project_id,task_id) VALUES ('tsw@uvigo.es',2,9);
+INSERT INTO users_on_tasks (user_mail,project_id,task_id) VALUES ('expedition33@expedition.fr',3,10);
 
 
 INSERT INTO categories (cat_name, cat_desc) VALUES ("Trabajo","Descripcion Trabajo");
