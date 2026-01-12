f2f8ea5f0bda686a8de54d310e1f54e906abc147
Aka <90091466+Akanenosketch@users.noreply.github.com>
2026-01-12 16:36:45 +0100

    Actualizar register.php

--- www/view/users/register.php
+++ www/view/users/register.php
@@ -11,7 +11,7 @@ $errors = $view->getVariable("errors");
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <link rel="stylesheet" type="text/css" href="./view/css/theme.css">
-    <link rel="stylesheet" type="text/css" href="./view/css/unifiedThemes.css">
+    <link rel="stylesheet" type="text/css" href="./view/css/users/register.css">
     <title><?= i18n("Form de Registro") ?></title>
 </head>
 
